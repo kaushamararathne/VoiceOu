49157c654964a03873088a643e32fe6e8ac4b46b
kaushamararathne <kaushamararathne@gmail.com>
2024-04-29 02:24:26 +0530

    2.23 29/4/2024

--- VoiceOU/settings.py
+++ VoiceOU/settings.py
@@ -36,8 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',    
+    'django.contrib.staticfiles',
+        
     'base.apps.BaseConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
